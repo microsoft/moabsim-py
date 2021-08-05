1424c2a60d0a738e2d70bfba8ce7e9f9b854c260
Journey McDowell <jomcdowe@microsoft.com>
2021-08-05 14:05:46 -0700

    Updating instance count to 20 instead of 30, defualt Moab values

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ def pytest_addoption(parser):
     parser.addoption("--concept_name", action="store", default="MoveToCenter")
     parser.addoption("--file_name", action="store", default="assess_config.json")
     parser.addoption("--simulator_package_name", action="store", default="Moab")
-    parser.addoption("--instance_count", action="store", default="30")
+    parser.addoption("--instance_count", action="store", default="20")
     parser.addoption("--custom_assess_name", action="store", default="my_custom_assessment")
     parser.addoption("--log_analy_workspace", action="store", default=None)
     parser.addoption("--inkling_fname", action="store", default="./Machine-Teaching-Examples/model_import/moab-imported-concept.ink")
